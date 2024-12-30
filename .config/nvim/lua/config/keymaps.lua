fb8f23eb7f277a0fea2edd090bd75e7529b56c24
heitorfreitasferreira <heitor.ff@hotmail.com>
2024-12-30 20:31:56 -0300

    Alterando lazygit e whichkey
    alterações pequenas visuais

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,3 +10,11 @@ end, { noremap = true, silent = true })
 -- Don't leave visual mode when changing indent
 vim.api.nvim_set_keymap("x", ">", ">gv", { noremap = true })
 vim.api.nvim_set_keymap("x", "<", "<gv", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "<leader>tw", ":Twilight<CR>", { noremap = true })
+
+-- Insert mode mappings
+vim.api.nvim_set_keymap("i", "<C-h>", "<Left>", {})
+vim.api.nvim_set_keymap("i", "<C-j>", "<Down>", {})
+vim.api.nvim_set_keymap("i", "<C-k>", "<Up>", {})
+vim.api.nvim_set_keymap("i", "<C-l>", "<Right>", {})
